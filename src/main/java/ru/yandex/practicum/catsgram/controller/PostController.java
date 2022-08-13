4f11c0609dc7a920961a8e6c9ea669d7335b4a84
Artemii <artemijpotapov1@yandex.ru>
2022-08-13 15:11:43 +0300

    feat: добавить эндпоинты для получения пользователей и постов по идентификаторам

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -3,15 +3,13 @@ package ru.yandex.practicum.catsgram.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PostController {
@@ -31,7 +29,14 @@ public class PostController {
         return postService.findAll();
     }
 
-    @PostMapping(value = "/post")
+    @GetMapping("/posts/post/{postId}")
+    public Optional<Post> findById(@PathVariable int postId) {
+        return postService.findAll().stream()
+                .filter(x -> x.getId() == postId)
+                .findFirst();
+    }
+
+    @PostMapping(value = "/posts/post")
     public Post create(@RequestBody Post post) {
         log.debug("Пост: {}", post);
         return postService.create(post);
--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -4,11 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.controller.exceptions.InvalidEmailException;
-import ru.yandex.practicum.catsgram.controller.exceptions.UserAlreadyExistException;
 import ru.yandex.practicum.catsgram.model.User;
 import ru.yandex.practicum.catsgram.service.UserService;
 
+import java.util.Optional;
 import java.util.Set;
 
 @RestController
@@ -28,6 +27,13 @@ public class UserController {
     public Set<User> findAll() {
         log.debug("Текущее количество пользователей: {}", userService.findAll().size());
         return userService.findAll();
+    }
+
+    @GetMapping("/user/{userEmail}")
+    public Optional<User> findByEmail(@PathVariable String userEmail) {
+        return userService.findAll().stream()
+                .filter(x -> x.getEmail().equals(userEmail))
+                .findFirst();
     }
 
     @PostMapping
