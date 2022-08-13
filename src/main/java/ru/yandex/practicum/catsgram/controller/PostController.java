4c3e2e1115a8f9ebae20784d4ccd9cfc87f4f4ba
Artemii <artemijpotapov1@yandex.ru>
2022-08-13 15:52:34 +0300

    feat: добавить форматирование, добавить сортировку

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -3,10 +3,12 @@ package ru.yandex.practicum.catsgram.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -24,14 +26,24 @@ public class PostController {
     private static final Logger log = LoggerFactory.getLogger(PostController.class);
 
     @GetMapping("/posts")
-    public List<Post> findAll() {
-        log.debug("Текущее количество постов: {}", postService.findAll().size());
-        return postService.findAll();
+    public List<Post> findAll(
+            @RequestParam(value = "page", defaultValue = "0", required = false) Integer page,
+            @RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
+            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort
+    ) {
+        if(!(sort.equals("asc") || sort.equals("desc"))){
+            throw new IllegalArgumentException();
+        }
+        if(page < 0 || size <= 0){
+            throw new IllegalArgumentException();
+        }
+        Integer from = page * size;
+        return postService.findAll(size, from, sort);
     }
 
     @GetMapping("/posts/post/{postId}")
     public Optional<Post> findById(@PathVariable int postId) {
-        return postService.findAll().stream()
+        return postService.findAbsAll().stream()
                 .filter(x -> x.getId() == postId)
                 .findFirst();
     }
--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -8,6 +8,7 @@ import ru.yandex.practicum.catsgram.model.Post;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
@@ -15,13 +16,29 @@ public class PostService {
     private final UserService userService;
     private final List<Post> posts = new ArrayList<>();
 
+    private static Integer globalId = 0;
+
     @Autowired
     public PostService(UserService userService) {
         this.userService = userService;
     }
 
-    public List<Post> findAll() {
+    public List<Post> findAll(Integer size, Integer from, String sort) {
+        return posts.stream().sorted((p0, p1) -> {
+            int comp = p0.getCreationDate().compareTo(p1.getCreationDate()); //прямой порядок сортировки
+            if(sort.equals("desc")){
+                comp = -1 * comp; //обратный порядок сортировки
+            }
+            return comp;
+        }).skip(from).limit(size).collect(Collectors.toList());
+    }
+
+    public List<Post> findAbsAll() {
         return posts;
+    }
+
+    private static Integer getNextId(){
+        return globalId++;
     }
 
     public Post create(Post post) {
@@ -30,5 +47,15 @@ public class PostService {
         }
         posts.add(post);
         return post;
+    }
+
+    public List<Post> findAllByUserEmail(String email, Integer size, String sort) {
+        return posts.stream().filter(p -> email.equals(p.getAuthor())).sorted((p0, p1) -> {
+            int comp = p0.getCreationDate().compareTo(p1.getCreationDate()); //прямой порядок сортировки
+            if(sort.equals("desc")){
+                comp = -1 * comp; //обратный порядок сортировки
+            }
+            return comp;
+        }).limit(size).collect(Collectors.toList());
     }
 }
